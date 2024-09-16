c6f63aab9b4387251151a577cc57c13442d9375d
Ben Gigliotti <bhg5096@psu.edu>
2024-09-16 16:10:55 -0400

    Someone Cooked Here

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "BenGigliotti";
+    char STUDENT_ID[] = "bhg5096@psu.edu";
 
     return 0 ;
 }
